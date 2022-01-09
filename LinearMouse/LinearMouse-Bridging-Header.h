6cf10d34caeff0e808ca2464b40053c55452c008
Jiahao Lu <lujjjh@gmail.com>
2022-01-09 12:00:36 +0800

    Handle device additions and removals (#50)

--- LinearMouse/LinearMouse-Bridging-Header.h
+++ LinearMouse/LinearMouse-Bridging-Header.h
@@ -5,4 +5,15 @@
 //  Created by lujjjh on 2021/8/5.
 //
 
+#include <IOKit/hidsystem/IOHIDEventSystemClient.h>
 #include "../Touch/TouchSimulate.h"
+
+typedef void (^IOHIDServiceClientBlock)(void *, void *, IOHIDServiceClientRef);
+
+IOHIDEventSystemClientRef IOHIDEventSystemClientCreate(CFAllocatorRef);
+void IOHIDEventSystemClientSetMatching(IOHIDEventSystemClientRef, CFDictionaryRef);
+void IOHIDEventSystemClientRegisterDeviceMatchingBlock(IOHIDEventSystemClientRef, IOHIDServiceClientBlock, void *, void *);
+void IOHIDEventSystemClientUnregisterDeviceMatchingBlock(IOHIDEventSystemClientRef);
+void IOHIDEventSystemClientScheduleWithDispatchQueue(IOHIDEventSystemClientRef, dispatch_queue_t);
+
+void IOHIDServiceClientRegisterRemovalBlock(IOHIDServiceClientRef, IOHIDServiceClientBlock, void*, void*);
