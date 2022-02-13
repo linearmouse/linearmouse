6987741650ad0e350c7fc542492d30130eda1277
Jiahao Lu <lujjjh@gmail.com>
2022-02-13 10:25:47 +0800

    Whitelisting Magic Mouse and Magic Mouse 2

--- LinearMouse/MouseDetector/ExperimentalMouseDetector.swift
+++ LinearMouse/MouseDetector/ExperimentalMouseDetector.swift
@@ -46,15 +46,31 @@ class ExperimentalMouseDetector: MouseDetector {
             guard let product = productRef as? String else {
                 return
             }
+            guard let vendorIDRef = IOHIDServiceClientCopyProperty(service, kIOHIDVendorIDKey as CFString) else {
+                return
+            }
+            guard let vendorID = vendorIDRef as? Int else {
+                return
+            }
+            guard let productIDRef = IOHIDServiceClientCopyProperty(service, kIOHIDProductIDKey as CFString) else {
+                return
+            }
+            guard let productID = productIDRef as? Int else {
+                return
+            }
             self.lastActiveService = service
             self.lastActiveServiceType = {
-                if IOHIDServiceClientConformsTo(service, UInt32(kHIDPage_Digitizer), UInt32(kHIDUsage_Dig_TouchPad)) != 0 {
-                    return .trackpad
+                if IOHIDServiceClientConformsTo(service, UInt32(kHIDPage_Digitizer), UInt32(kHIDUsage_Dig_TouchPad)) == 0 {
+                    return .mouse
                 }
-                return .mouse
+                if (vendorID == 0x004c || vendorID == 0x05ac) && (productID == 0x0269 || productID == 0x030d) {
+                    return .mouse
+                }
+                return .trackpad
             }()
-            os_log("switched to: %{public}@, reported as %{public}@", log: ExperimentalMouseDetector.log, type: .debug,
-                   product, String(describing: self.lastActiveServiceType))
+            os_log("switched to: %{public}@ (vid=0x%{public}04x, pid=0x%{public}04x), type: %{public}@",
+                   log: ExperimentalMouseDetector.log, type: .debug,
+                   product, vendorID, productID, String(describing: self.lastActiveServiceType))
         }, nil, nil)
         IOHIDEventSystemClientScheduleWithDispatchQueue(client, DispatchQueue.main)
     }
