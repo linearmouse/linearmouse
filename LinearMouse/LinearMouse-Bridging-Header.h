c6503bcb356e6299b75a4c4eac22f7ab90d2690e
Jiahao Lu <lujjjh@gmail.com>
2022-02-17 15:36:03 +0800

    Rework reverse scrolling (#87)

--- LinearMouse/LinearMouse-Bridging-Header.h
+++ LinearMouse/LinearMouse-Bridging-Header.h
@@ -25,3 +25,6 @@ void IOHIDServiceClientRegisterRemovalBlock(IOHIDServiceClientRef, IOHIDServiceC
 
 typedef void (*IOHIDEventSystemClientPropertyChangedCallback)(void* target, void* context, CFStringRef property, CFTypeRef value);
 void IOHIDEventSystemClientRegisterPropertyChangedCallback(IOHIDEventSystemClientRef client, CFStringRef property, IOHIDEventSystemClientPropertyChangedCallback callback, void* target, void *context);
+
+int _CGSDefaultConnection(void);
+void CGSSetSwipeScrollDirection(int cid, BOOL dir);
