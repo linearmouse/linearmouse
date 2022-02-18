88ac0d8aa954dd3ada47f5f541f4da239c536b75
Jiahao Lu <lujjjh@gmail.com>
2022-02-18 13:51:00 +0800

    Add settings for auto update

--- LinearMouse/UI/CheckForUpdatesButton.swift
+++ LinearMouse/UI/CheckForUpdatesButton.swift
@@ -13,14 +13,40 @@ final class UpdaterViewModel: ObservableObject {
     static let shared = UpdaterViewModel()
 
     @Published var canCheckForUpdates = false
+    @Published var automaticallyChecksForUpdates = false {
+        didSet {
+            controller.updater.automaticallyChecksForUpdates = automaticallyChecksForUpdates
+        }
+    }
+    @Published var updateCheckInterval: TimeInterval = 604800 {
+        didSet {
+            controller.updater.updateCheckInterval = updateCheckInterval
+        }
+    }
 
     private let controller = AutoUpdateManager.shared.controller
-    private var subscription: AnyCancellable! = nil
+    private var canCheckForUpdatesSubscription: AnyCancellable! = nil
+    private var automaticallyChecksForUpdatesSubscription: AnyCancellable! = nil
+    private var updateCheckIntervalSubscription: AnyCancellable! = nil
 
     init() {
-        subscription = controller.updater.publisher(for: \.canCheckForUpdates)
+        canCheckForUpdatesSubscription = controller.updater.publisher(for: \.canCheckForUpdates)
             .sink { value in
                 self.canCheckForUpdates = value
+            }
+        automaticallyChecksForUpdatesSubscription = controller.updater.publisher(for: \.automaticallyChecksForUpdates)
+            .sink { value in
+                guard value != self.automaticallyChecksForUpdates else {
+                    return
+                }
+                self.automaticallyChecksForUpdates = value
+            }
+        updateCheckIntervalSubscription = controller.updater.publisher(for: \.updateCheckInterval)
+            .sink { value in
+                guard value != self.updateCheckInterval else {
+                    return
+                }
+                self.updateCheckInterval = value
             }
     }
 
@@ -35,5 +61,18 @@ struct CheckForUpdatesButton: View {
     var body: some View {
         Button("Check for Updates...", action: updaterViewModel.checkForUpdates)
             .disabled(!updaterViewModel.canCheckForUpdates)
+        HStack {
+            Toggle(isOn: $updaterViewModel.automaticallyChecksForUpdates) {
+                Text("Automatically")
+            }
+            Picker("", selection: $updaterViewModel.updateCheckInterval) {
+                Text("Daily").tag(TimeInterval(86400))
+                Text("Weekly").tag(TimeInterval(604800))
+                Text("Monthly").tag(TimeInterval(2629800))
+            }
+            .frame(width: 120)
+            .disabled(!updaterViewModel.automaticallyChecksForUpdates)
+        }
+        .controlSize(.small)
     }
 }
