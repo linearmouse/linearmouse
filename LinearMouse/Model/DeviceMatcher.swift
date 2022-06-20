2fcfa5c47daa36a4bb2014cfb4cb54e290f8b76b
Jiahao Lu <lujjjh@gmail.com>
2022-06-21 01:21:40 +0800

    Add `category` for device matching

--- LinearMouse/Model/DeviceMatcher.swift
+++ LinearMouse/Model/DeviceMatcher.swift
@@ -3,10 +3,15 @@
 
 import Defaults
 
-struct DeviceMatcher: Codable, Defaults.Serializable, Equatable {
+struct DeviceMatcher: Codable, Defaults.Serializable {
     let vendorID: HexValue?
     let productID: HexValue?
     let serialNumber: String?
+    let category: ArrayOrSingleValue<Category>?
+
+    enum Category: String, Codable {
+        case mouse, trackpad
+    }
 }
 
 extension DeviceMatcher {
@@ -14,8 +19,9 @@ extension DeviceMatcher {
         let vendorID = HexValue(device.vendorID)
         let productID = HexValue(device.productID)
         let serialNumber = device.serialNumber
+        let category = ArrayOrSingleValue(value: [Category(from: device.category)])
 
-        self.init(vendorID: vendorID, productID: productID, serialNumber: serialNumber)
+        self.init(vendorID: vendorID, productID: productID, serialNumber: serialNumber, category: category)
     }
 
     func match(with device: Device) -> Bool {
@@ -25,10 +31,38 @@ extension DeviceMatcher {
 
         guard matchValue(vendorID?.value, device.vendorID),
               matchValue(productID?.value, device.productID),
-              matchValue(serialNumber, device.serialNumber) else {
+              matchValue(serialNumber, device.serialNumber)
+        else {
             return false
         }
 
+        if let category = category {
+            guard category.value.contains(where: { $0.deviceCategory == device.category })
+            else {
+                return false
+            }
+        }
+
         return true
+    }
+}
+
+extension DeviceMatcher.Category {
+    init(from deviceCategory: Device.Category) {
+        switch deviceCategory {
+        case .mouse:
+            self = .mouse
+        case .trackpad:
+            self = .trackpad
+        }
+    }
+
+    var deviceCategory: Device.Category {
+        switch self {
+        case .mouse:
+            return .mouse
+        case .trackpad:
+            return .trackpad
+        }
     }
 }
