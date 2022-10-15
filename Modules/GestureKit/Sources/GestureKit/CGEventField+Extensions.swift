352609259267ab0f9bfff6c06e4adeb18c032bc5
Jiahao Lu <lujjjh@gmail.com>
2022-10-15 11:56:28 +0800

    Fix lint

--- Modules/GestureKit/Sources/GestureKit/CGEventField+Extensions.swift
+++ Modules/GestureKit/Sources/GestureKit/CGEventField+Extensions.swift
@@ -3,14 +3,16 @@
 
 import CoreGraphics
 
-/// - SeeAlso: https://github.com/WebKit/WebKit/blob/ab59722dc517c798f7d88bfe4dcb7b33b8473e7e/Tools/TestRunnerShared/spi/CoreGraphicsTestSPI.h#L39
+// - SeeAlso:
+// https://github.com/WebKit/WebKit/blob/ab59722dc517c798f7d88bfe4dcb7b33b8473e7e/Tools/TestRunnerShared/spi/CoreGraphicsTestSPI.h#L39
 extension CGEventField {
     static let gestureHIDType = Self(rawValue: 110)!
     static let gestureSwipeValue = Self(rawValue: 115)!
     static let gesturePhase = Self(rawValue: 132)!
 }
 
-/// - SeeAlso: https://github.com/WebKit/WebKit/blob/ab59722dc517c798f7d88bfe4dcb7b33b8473e7e/Tools/TestRunnerShared/spi/CoreGraphicsTestSPI.h#L87
+// - SeeAlso:
+// https://github.com/WebKit/WebKit/blob/ab59722dc517c798f7d88bfe4dcb7b33b8473e7e/Tools/TestRunnerShared/spi/CoreGraphicsTestSPI.h#L87
 enum CGSGesturePhase: UInt8 {
     case none = 0
     case began = 1
@@ -20,7 +22,8 @@ enum CGSGesturePhase: UInt8 {
     case mayBegin = 128
 }
 
-/// - SeeAlso: https://github.com/WebKit/WebKit/blob/52d85940c6acce0f6b25fe1f8155c25283058e27/Source/WebCore/PAL/pal/spi/mac/IOKitSPIMac.h#L74
+// - SeeAlso:
+// https://github.com/WebKit/WebKit/blob/52d85940c6acce0f6b25fe1f8155c25283058e27/Source/WebCore/PAL/pal/spi/mac/IOKitSPIMac.h#L74
 enum IOHIDEventType: UInt32 {
     case none
     case vendorDefined
@@ -33,7 +36,7 @@ enum IOHIDEventType: UInt32 {
     case force = 32
 }
 
-/// - SeeAlso: https://opensource.apple.com/source/IOHIDFamily/IOHIDFamily-368.13/IOHIDFamily/IOHIDEventTypes.h.auto.html
+// - SeeAlso: https://opensource.apple.com/source/IOHIDFamily/IOHIDFamily-368.13/IOHIDFamily/IOHIDEventTypes.h.auto.html
 public enum IOHIDSwipeMask: UInt32 {
     case swipeUp = 0x01
     case swipeDown = 0x02
