fa745ab00eea33e92ddcd620167b0ca455cd2442
Jiahao Lu <lujjjh@gmail.com>
2023-03-27 10:23:17 +0800

    Update

---
+++ LinearMouse/UI/ButtonsSettings/ButtonMappingsSection/ButtonMappingEditSheet.swift
@@ -0,0 +1,12 @@
+// MIT License
+// Copyright (c) 2021-2023 Jiahao Lu
+
+import SwiftUI
+
+struct ButtonMappingEditSheet: View {
+    @Binding var mapping: Scheme.Buttons.Mapping
+
+    var body: some View {
+        EmptyView()
+    }
+}
