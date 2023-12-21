bd859a290dc5e9615e27676cd619f597709a5ef1
Jiahao Lu <lujjjh@gmail.com>
2023-12-21 21:16:04 +0800

    macOS hangs when Accessibility Permission being revoked while running (#669)
    Fixes #667.

---
+++ LinearMouse/EventTap/GlobalEventTapWatchdog.swift
@@ -0,0 +1,43 @@
+// MIT License
+// Copyright (c) 2021-2023 LinearMouse
+
+import AppKit
+import os.log
+
+class GlobalEventTapWatchdog {
+    private static let log = OSLog(subsystem: Bundle.main.bundleIdentifier!, category: "GlobalEventTapWatchdog")
+
+    init() {}
+
+    deinit {
+        stop()
+    }
+
+    var timer: Timer?
+
+    func start() {
+        timer = Timer.scheduledTimer(withTimeInterval: 10, repeats: true) { [weak self] _ in
+            guard let self = self else {
+                return
+            }
+
+            self.testAccessibilityPermission()
+        }
+    }
+
+    func stop() {
+        timer?.invalidate()
+        timer = nil
+    }
+
+    func testAccessibilityPermission() {
+        do {
+            try EventTap.observe([.scrollWheel]) { _, event in
+                event
+            }.removeLifetime()
+        } catch {
+            stop()
+            Application.restart()
+        }
+    }
+}
--- LinearMouse/UI/AccessibilityPermissionWindow.swift
+++ LinearMouse/UI/AccessibilityPermissionWindow.swift
@@ -48,20 +48,6 @@ extension AccessibilityPermissionWindow: NSWindowDelegate {
             exit(0)
         }
 
-        let url = URL(fileURLWithPath: Bundle.main.resourcePath!)
-        let path = url.deletingLastPathComponent().deletingLastPathComponent().absoluteString
-        let task = Process()
-        task.launchPath = "/bin/sh"
-        task.environment = ["BUNDLE_PATH": path]
-        task.arguments = [
-            "-c",
-            "while $(kill -0 $PPID 2>/dev/null); do sleep .1; done; /usr/bin/open \"$BUNDLE_PATH\" --args --show"
-        ]
-        do {
-            try task.run()
-        } catch {
-            NSAlert(error: error).runModal()
-        }
-        NSApp.terminate(nil)
+        Application.restart()
     }
 }
