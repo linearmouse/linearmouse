a97aebf986e7afb5fc93d5d3765ba5dfaafd15de
Jiahao Lu <lujjjh@gmail.com>
2026-03-09 22:13:51 +0800

    Revert "Fix tracking speed not working for Trackpad and Magic Mouse on macOS …"
    This reverts commit fc305e9e71837afbdb388b82cbdd2af26cab4da6.

--- Modules/PointerKit/Sources/PointerKit/PointerDevice.swift
+++ Modules/PointerKit/Sources/PointerKit/PointerDevice.swift
@@ -38,11 +38,6 @@ public class PointerDevice {
         this.inputReportCallback(Data(bytes: report, count: reportLength))
     }
 
-    // Some devices like Magic Mouse do not return an HIDUseLinearScalingMouseAcceleration on macOS 26.
-    // So we have to store it manually.
-    // TODO: Use system global value as the initial value.
-    private var _useLinearScalingMouseAcceleration = 0
-
     init(_ client: IOHIDServiceClient) {
         self.client = client
         device = client.device
@@ -168,20 +163,12 @@ public extension PointerDevice {
 
     var useLinearScalingMouseAcceleration: Int? {
         get {
-            guard #available(macOS 14, *) else {
-                return 0
-            }
-
-            return client.getProperty(kIOHIDUseLinearScalingMouseAccelerationKey) ?? _useLinearScalingMouseAcceleration
+            // TODO: Use `kIOHIDUseLinearScalingMouseAccelerationKey`.
+            client.getProperty("HIDUseLinearScalingMouseAcceleration")
         }
         set {
-            guard #available(macOS 14, *) else {
-                return
-            }
-
-            if let newValue, client.setProperty(newValue, forKey: kIOHIDUseLinearScalingMouseAccelerationKey) {
-                _useLinearScalingMouseAcceleration = newValue
-            }
+            // TODO: Use `kIOHIDUseLinearScalingMouseAccelerationKey`.
+            client.setProperty(newValue, forKey: "HIDUseLinearScalingMouseAcceleration")
         }
     }
 
